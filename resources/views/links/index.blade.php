8fb2052f687a0da4db51c8bb8acee108b191586f
tsf <tsf@gmail.com>
2022-08-20 13:29:28 +0100

    redesigned links page from compaigns

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -12,47 +12,74 @@
                     </div>
                     <div class="my-4 row">
                         @foreach($links as $link)
-                        <div class=" mb-3 col-md-6 col-sm-12">
+                        <div class=" mb-3 col-sm-12">
                             <div class="card shadow-sm border-0 bg-white rounded">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class=" col-md-3">
-                                        <div class="text-center">
-                                            {!! QrCode::size(130)->color(0, 87, 163)->generate('https://taysoufcrypto.com/visit/'.$compaign->id.'/'.$link->id) !!}
-                                        </div>
-                                        <div class="text-center bg-success mt-2 rounded text-white">
-                                            Scan me 
-                                        </div>
-                                    </div>
-                                    {{-- <div class="bg-primary col-md-4">test1</div> --}}
-                                    <div class="col-md-9">
-                                        <div class="mb-3">
-                                            <span class="badge rounded-pill bg-info text-dark px-3">URL</span>
-                                        </div>
-                                        <div class="fw-bolder h5">
-                                            {{$link->name}}
-                                        </div>
-                                        <div class="row">
-                                            <div class="text-xs ">
-                                                <span class="">Created: {{ $link->created_at }}</span>
-                                                <span class="ms-4">
-                                                    <img width="20" class="mb-1" src = "{{ asset('/images/folder_compaign.svg') }}" />
-                                                    {{ $compaign->name }}
-                                                </span>
+                               <div class="row ">
+                                    <div class="col-5 h4">
+                                        <div>
+                                            <div class="mb-3">
+                                                <span class="badge rounded-pill bg-info text-dark px-3">URL</span>
                                             </div>
-                                            <div class="mt-3">
-                                                <div class="d-flex align-items-center">
-                                                    <span>{{ $link->visits_count }} Scans</span>
-                                                    <span class="ms-3">
-                                                            <a href="/dashboard/compaigns/{{ $compaign->id }}/links/{{ $link->id }}/details" class="link-unstyled">
-                                                                <i class="fs-2 bi bi-arrow-right mt-5"></i>
-                                                            </a>
+                                            <div class="fw-bolder h5">
+                                                {{$link->name}}
+                                            </div>
+                                            <div class="row mt-3">
+                                                <div class="text-xs ">
+                                                    <span class="">Created: {{ $link->created_at }}</span>
+                                                    <span class="ms-4">
+                                                        <img width="20" class="mb-1" src = "{{ asset('/images/folder_compaign.svg') }}" />
+                                                        {{ $compaign->name }}
                                                     </span>
                                                 </div>
+                                                {{-- <div class="mt-3">
+                                                    <div class="d-flex align-items-center">
+                                                        <span>{{ $link->visits_count }} Scans</span>
+                                                        <span class="ms-3">
+                                                                <a href="/dashboard/compaigns/{{ $compaign->id }}/links/{{ $link->id }}/details" class="link-unstyled">
+                                                                    <i class="fs-2 bi bi-arrow-right mt-5"></i>
+                                                                </a>
+                                                        </span>
+                                                    </div>
+                                                </div> --}}
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                    <div class="col-7">
+                                        <div class="row d-flex align-items-center">
+                                            <div class="col-md-4 text-center ">
+                                                <div class="col ">
+                                                    <div class="h3">
+                                                        {{ $link->visits_count }}
+                                                    </div>
+                                                    <div class="">
+                                                        Scans
+                                                    </div>
+                                                    <div class="mt-3 ">
+                                                        <a href="/dashboard/compaigns/{{ $compaign->id }}/links/{{ $link->id }}/details" class="">
+                                                            <div class="d-flex align-items-center justify-content-center">
+                                                                <div>Details</div>
+                                                                <div class="ms-2"><i class="fs-2 bi bi-arrow-right"></i></div>
+                                                            </div>
+                                                        {{-- details --}}
+                                                            {{-- <i class="fs-2 bi bi-arrow-right"></i> --}}
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 text-center qr-container " >
+                                                <div class="text-center qr-code">
+                                                    {!! QrCode::size(120)->color(0, 0, 0)->generate('https://taysoufcrypto.com/visit/'.$compaign->id.'/'.$link->id) !!}
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 text-center ">
+                                                <button class="btn btn-secondary">
+                                                    Download
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                               </div>
                             </div>
                         </div>
                         </div> 
